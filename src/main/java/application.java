b654d621dacda6f3f8d85547ec31b64e8e58d6e3
Sanjay <ks0077sanjay.gmail.com>
2021-04-17 18:19:35 +0530

    add new file

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,6 +1,7 @@
 public class application {
     public static void main(String[] args) {
         System.out.println("HI world");
+        System.out.println("Changes");
         System.out.println("added git to the local repository");
     }
 }
