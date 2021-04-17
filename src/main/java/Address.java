0a937bdb835691de3e40fd11c3c5e067525923c3
Sanjay <ks0077sanjay.gmail.com>
2021-04-17 19:50:19 +0530

    adding JPA annotation

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,8 +1,76 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ADDRESS")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
     private int addressID;
+    @Column(name = "CITY")
     private String city;
+    @Column(name = "STATE")
     private String state;
+    @Column(name = "COUNTRY")
     private String country;
+    @Column(name = "PIN_CODE")
     private String pinCode;
 
+    public Address(int addressID, String city, String state, String country, String pinCode) {
+        this.addressID = addressID;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pinCode = pinCode;
+    }
+
+    public int getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(int addressID) {
+        this.addressID = addressID;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressID=" + addressID +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", pinCode='" + pinCode + '\'' +
+                '}';
+    }
 }
--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -1,2 +1,48 @@
+import javax.persistence.*;
+
+@Entity
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int departID;
+    @Column(name = "DEPARTMENT")
+    private String department;
+    @Column(name = "OPERATED_REGION")
+    private String operatedRegion;
+
+    public Department() {
+    }
+
+    public int getDepartID() {
+        return departID;
+    }
+
+    public void setDepartID(int departID) {
+        this.departID = departID;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getOperatedRegion() {
+        return operatedRegion;
+    }
+
+    public void setOperatedRegion(String operatedRegion) {
+        this.operatedRegion = operatedRegion;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departID=" + departID +
+                ", department='" + department + '\'' +
+                ", operatedRegion='" + operatedRegion + '\'' +
+                '}';
+    }
 }
