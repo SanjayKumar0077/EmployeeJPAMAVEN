97d0c73c92cfb719da4c1daba1c65e7993e6cd40
Sanjay <ks0077sanjay.gmail.com>
2021-04-17 19:18:54 +0530

    adding enitiy class

---
+++ src/main/java/Address.java
@@ -0,0 +1,8 @@
+public class Address {
+    private int addressID;
+    private String city;
+    private String state;
+    private String country;
+    private String pinCode;
+
+}
---
+++ src/main/java/EmployeeMaster.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class EmployeeMaster  {
+    private int empID;
+    private String password;
+    private int empAge;
+    private double empCTC;
+    private boolean empActive;
+    private Department department;
+    private Address address;
+}
