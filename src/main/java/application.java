8f630505e50f25d31f76216ef133630e7b06843d
Sanjay <ks0077sanjay.gmail.com>
2021-04-17 18:18:11 +0530

    sanjay

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,5 +1,6 @@
 public class application {
     public static void main(String[] args) {
         System.out.println("HI world");
+        System.out.println("added git to the local repository");
     }
 }
