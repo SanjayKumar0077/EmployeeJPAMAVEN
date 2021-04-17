21bae52b82202e4c19b94dee097da1be32e68740
Sanjay <ks0077sanjay.gmail.com>
2021-04-17 22:37:01 +0530

    added entity manager and save method

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,5 +1,35 @@
-public class application {
-    public static void main(String[] args) {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
+public class application {
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory("EmployeeProject");
+
+    public static void main(String[] args) {
+        EmployeeMaster employeeMaster=new EmployeeMaster();
+       // employeeMaster.setDepartment(d);
+
+        saveEmployee(employeeMaster);
+        entityManagerFactory.close();
+    }
+
+    public static String saveEmployee(EmployeeMaster employeeMaster) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        try {
+            entityTransaction.begin();
+            entityManager.persist(employeeMaster);
+            entityTransaction.commit();
+        } catch (Exception exception) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            exception.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return "Save Employee successfully";
     }
 }
